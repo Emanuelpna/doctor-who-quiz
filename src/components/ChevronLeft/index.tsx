a1574e8ffb33d735f96797ee35cd53cc2e0c581d
Emanuel Andrade <emanuelpna@yahoo.com.br>
2021-01-26 13:39:27 -0300

    Adicionando ícone do Heroicons

---
+++ src/components/ChevronLeft/index.tsx
@@ -0,0 +1,18 @@
+// https://heroicons.com/
+const ChevronLeftIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M15 19l-7-7 7-7"
+    />
+  </svg>
+)
+
+export default ChevronLeftIcon
