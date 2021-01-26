8dbe63fc3e42d7c7fa5a0f2b3722aca34c0fa89a
Emanuel Andrade <emanuelpna@yahoo.com.br>
2021-01-26 13:39:15 -0300

    Adicionando props padrão do elemento button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,15 @@
+import { ButtonHTMLAttributes } from 'react'
 import * as S from './styled'
 
 type ButtonProps = {
   children: React.ReactNode
   disabled?: boolean
-}
+} & ButtonHTMLAttributes<HTMLButtonElement>
 
-const Button = ({ children, disabled = false }: ButtonProps) => (
-  <S.Container disabled={disabled}>{children}</S.Container>
+const Button = ({ children, disabled = false, ...props }: ButtonProps) => (
+  <S.Container disabled={disabled} {...props}>
+    {children}
+  </S.Container>
 )
 
 export default Button
