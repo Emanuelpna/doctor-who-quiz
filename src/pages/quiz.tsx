25d415afe15d1c005e78965459b27c22e48d1926
Emanuel Andrade <emanuelpna@yahoo.com.br>
2021-01-28 14:20:22 -0300

    Buscando Perguntas da API

--- src/pages/quiz.tsx
+++ src/pages/quiz.tsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from 'react'
+
 import db from '../../db.json'
 
 import Layout from 'components/Layout'
 import Quiz from 'templates/Quiz'
 
 export default function Home() {
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    const getMyQuestions = async () => {
+      const response = await fetch('/api/db')
+
+      const myDatabase = await response.json()
+
+      setQuestions(myDatabase.questions)
+    }
+
+    getMyQuestions()
+  }, [])
+
   return (
     <Layout imageBg={db.bg} bgCopy={db['bg-copy']}>
-      <Quiz questions={db.questions} />
+      <Quiz questions={questions} />
     </Layout>
   )
 }
