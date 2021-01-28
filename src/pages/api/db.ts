5d3a95d07d38711d59dea6170660c808650e0133
Emanuel Andrade <emanuelpna@yahoo.com.br>
2021-01-28 14:18:55 -0300

    Expondo db.json via API

---
+++ src/pages/api/db.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import db from '../../../db.json'
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  )
+
+  res.json(db)
+}
